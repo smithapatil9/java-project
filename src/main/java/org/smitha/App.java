c3bc67f44bb01a2baa9ba9760946f3d120ce1f39
smithapatil9 <smita.aptil859@gmail.com>
2017-12-19 14:06:47 +0800

    Basic files added

---
+++ src/main/java/org/smitha/App.java
@@ -0,0 +1,14 @@
+package org.smitha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Java Project");
+    }
+}
